eaf3a3df09293b475d5b32a9822724e0d01255d1
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-02-03 08:44:39 +0000

    [add]follow(model,controller,)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
     devise_for :users
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create,:destroy]
     resources :book_comments, only: [:create,:destroy]
+  end
+  
+  resources :user do
+    resource :relationships, only: [:create,:destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followes" => "relationships#followers", as: "followers"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
