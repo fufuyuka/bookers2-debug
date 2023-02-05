e6c8825b9e7f6309380f6df2c2dfcdaf2198c68e
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-02-05 13:58:23 +0000

    [add]favorite非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,12 +6,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save!
-    redirect_to book_path(book)
+    redirect_to book_path(book) 
   end
   
   def destroy
     BookComment.find(params[:id]).destroy
-     redirect_back(fallback_location: root_path)
+     redirect_back(fallback_location: root_path) #非同期化したらルート不要になる(覚えたら消す)
   end
   
   private
