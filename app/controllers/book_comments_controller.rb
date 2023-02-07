a855a450d4aa379862e46c03367bffa984172ef7
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-02-07 06:58:26 +0000

    [add]commnent非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,16 +2,15 @@ class BookCommentsController < ApplicationController
   before_action :ensure_correct_user, only: [:destroy]
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
   
   def destroy
-    BookComment.find(params[:id]).destroy
-     redirect_back(fallback_location: root_path) #非同期化したらルート不要になる(覚えたら消す)
+    @comment = BookComment.find(params[:id])
+   @comment.destroy
   end
   
   private
@@ -23,7 +22,6 @@ class BookCommentsController < ApplicationController
     def ensure_correct_user
       @book_comment = BookComment.find(params[:id])
       unless @book_comment.user == current_user
-        redirect_to user_path(current_user)
       end
     end
 
