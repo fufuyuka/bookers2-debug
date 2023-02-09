2906dffdc5bb79daa8607e3fdf5697c97c7eea8e
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-02-09 05:48:39 +0000

    [update]chat

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,12 +6,12 @@ class RoomsController < ApplicationController
       user_room.room 
     end
     
-    room = UserRoom.where(room_id: current_users_rooms, user_id: params[:user_id])[0].room
+    room = Room.where(room_id: current_users_rooms, user_id: params[:user_id])[0].room
      #条件に合うチャットルームが存在しない場合、新規作成
     if room.blank? 
-      room = UserRoom.create
-      UserRoom.create(user_id: current_user.id, room_id: room.id)
-      UserRoom.create(user_id: params[:user_id], room_id: room.id)
+      room = Room.create
+      Room.create(user_id: current_user.id, room_id: room.id)
+      Room.create(user_id: params[:user_id], room_id: room.id)
     end
     redirect_to room_path(room)
   end
