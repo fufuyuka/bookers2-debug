22b86c35480fbf57009ae87b232892142483d7d0
fufuyuka <121597200+fufuyuka@users.noreply.github.com>
2023-02-13 06:56:38 +0000

    [add]7days-createbooks

--- app/models/book.rb
+++ app/models/book.rb
@@ -19,6 +19,12 @@ class Book < ApplicationRecord
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 前週
   
+  #過去1週間分の日ごとの投稿数
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_two_days_ago, -> { where(created_at: 2.days.ago.all_day) }
+  scope :created_three_days_ago, -> { where(created_at: 3.days.ago.all_day) }
+  
   # 検索方法の分岐
   def self.looks(search, word)
     if search == "perfect_match"
